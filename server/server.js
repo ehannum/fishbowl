e6195814e9b1852b4ff4fd784b3885e6a35d8274
ehannum <ehannum@gmail.com>
2015-02-02 01:09:50 -0800

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
 
 // -- SERVE STARIC FILES
 
 app.use(express.static('public'));
 
+// -- SOCKET.IO
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.');
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.');
+  });
+});
+
+// -- START SERVER
+
 var port = process.env.PORT || 3030;
 console.log('Listening on port', port);
-app.listen(port);
+http.listen(port);
